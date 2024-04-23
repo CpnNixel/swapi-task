d4afd9270a497e348449da1fb7fe70f917828d75
mykyta.kysil <mykyta.kysil@charge-amps.com>
2024-04-23 05:21:19 +0200

    Added rate limiter

--- Swapi.Api/Program.cs
+++ Swapi.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.RateLimiting;
 using Swapi.Api.Modules;
 using Swapi.Data;
 
@@ -8,13 +9,39 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    options.AddPolicy(
+        "ApiCorsPolicy",
+        policyBuilder =>
+        {
+            policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    )
+);
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddTokenBucketLimiter(
+        "TokenBucket",
+        opt =>
+        {
+            opt.TokenLimit = 10_000;
+            opt.TokensPerPeriod = 10_000;
+            opt.ReplenishmentPeriod = TimeSpan.FromDays(1);
+        }
+    );
+});
 
 var app = builder.Build();
 
+app.UseCors("ApiCorsPolicy");
+app.UseRateLimiter();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGroup("Root").RootModule().WithTags("Root");
+app.MapGroup("Root").RootModule().WithName("Root")
+    .RequireRateLimiting("TokenBucket");
+
 app.MapGroup("Characters").CharactersModule().WithTags("Character");
 app.MapGroup("Films").FilmsModule().WithTags("Film");
 app.MapGroup("Planets").PlanetsModule().WithTags("Planet");
